535c0e73ab0ddd946c23f011791f474a0de0fa56
Hagin Onyango <hagincubes@gmail.com>
2016-03-25 14:39:00 -0400

    Updated from Brython Server: March 25, 2016 2:39:01 PM EDT

--- cryptography.py
+++ cryptography.py
@@ -12,4 +12,20 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 
 
-question = input("Press e to encypt and d to decript ")
+
+
+def encrypt(string, key):
+    messageNumber = [associations.find(a) for a in message]
+    keyNumber = [associations.find(b) for b in key] 
+    newvalarr = []
+    c = 0
+    for x in messageNumber:
+        newvalarr.append(x + keyNumber(c))
+        c += 1
+    return [associations[d] for d in newvalarr]
+    
+question = input("what is the message?")
+questionKey = input("what is the key?")
+print(encrypt(question, questionKey))
+
+
